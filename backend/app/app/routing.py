891c00ed4d0b5a48e33b775bdf7e28e746a29965
Stuart Rapoport <srapoport@dial-once.com>
2024-04-16 12:01:45 +0200

    fix https asgi

--- backend/app/app/routing.py
+++ backend/app/app/routing.py
@@ -1,6 +1,7 @@
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 import game.routing
 
