3eb068039a2ce07c3da0c87e4d5433f93584b119
MrMobbi <julliatmathias06@gmail.com>
2024-04-16 11:43:22 +0200

    problem fixed, Wrong path used from the chat app

--- backend/app/app/routing.py
+++ backend/app/app/routing.py
@@ -1,15 +1,10 @@
 
 from channels.auth import AuthMiddlewareStack
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
-import chat.routing
 import game.routing
 
-from app.middleware import TokenAuthMiddlewareStack
-
 print("ASGI SETTINGS_GAME: ", game.routing.websocket_urlpatterns)
-print("ASGI SETTINGS_CHAT: ", chat.routing.websocket_urlpatterns)
 
 """
 application = ProtocolTypeRouter({
