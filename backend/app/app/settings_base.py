b8c2543529a9ae4e4361d4dc1c235e62cdad7abe
lorobert <lorobert@student.42lausanne.ch>
2024-04-17 11:40:52 +0200

    Separate Django settings for dev and prod

--- backend/app/app/settings_base.py
+++ backend/app/app/settings_base.py
@@ -20,22 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
-SIMPLE_JWT = {
-    'SIGNING_KEY': config('SIGNING_KEY'),
-}
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-
 CSRF_TRUSTED_ORIGINS = [
     'https://localhost:8080'
-]
-
-ALLOWED_HOSTS = [
-    'localhost'
 ]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
