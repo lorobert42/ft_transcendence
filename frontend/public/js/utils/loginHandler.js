1044fd4b819be55024fcb730a75a8b78ab61a2c2
lorobert <lorobert@student.42lausanne.ch>
2024-04-17 10:13:17 +0200

    Use refresh token if access token is expired Improve JWT security in backend

--- frontend/public/js/utils/loginHandler.js
+++ frontend/public/js/utils/loginHandler.js
@@ -1,30 +1,56 @@
 
 export function isLoggedIn() {
-    let isLogged = localStorage.getItem('authToken') !== null && localStorage.getItem('authToken') !== undefined;
-    if(!isLogged) {
-        return {'value': false};
+    const authToken = localStorage.getItem('authToken');
+    const isLogged = authToken !== null &&
+        authToken !== undefined &&
+        !isTokenExpired(authToken);
+    if (isLogged) {
+        return true;
     }
-    let ret = {value:false, avatarURL:''};
-    fetch('api/user/me/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + localStorage.getItem('authToken')
-    }})
-    .then(response => {
-        if(!response.ok){
-            throw new Error('User Not Logged In');
-        }
-        return response.json();
-    })
-    .then(data => {
-        ret.value = true
-        ret.avatarURL = 'https://tenor.com/view/raccoon-ayasan-gif-24417811';
-    })
-    .catch(error => {
-        console.log(error);
-        return false;
-    });
-    return ret;
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (refreshToken !== null &&
+        refreshToken !== undefined &&
+        !isTokenExpired(refreshToken)
+    ) {
+        fetch("/api/user/token/refresh/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ refresh: refreshToken }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (Object.hasOwn(data, "access")) {
+                    localStorage.setItem("authToken", data.access);
+                    return true;
+                } else {
+                    return false;
+                }
+            })
+            .catch((error) => {
+                console.error("Login error:", error);
+                // Handle login error, e.g., show error message
+            });
+    }
+    return false;
+}
+
+function decodeJWT(token) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const payload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(payload);
+}
+
+function isTokenExpired(token) {
+    const decodedToken = decodeJWT(token);
+    const expirationTime = decodedToken.exp;
+    const currentTime = Math.floor(Date.now() / 1000);
+
+    return expirationTime <= currentTime;
 }
 
