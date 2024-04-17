8857a03b9a4cc8e73244d92ddce58a07a074753a
trubat-d <theotime2015@gmail.com>
2024-04-17 14:00:40 +0200

    trying to request users

--- frontend/public/pages/contacts.js
+++ frontend/public/pages/contacts.js
@@ -1,7 +1,9 @@
 export default function contacts() {
 
+    console.log(localStorage.getItem("authToken"));
     let users = fetch('https://localhost:8080/api/user/users/', {
         mode: "cors",
+        authToken: localStorage.getItem("authToken"),
     }).then(response => response.json())
     .then(data => {
         console.log(data);
@@ -13,17 +15,5 @@ export default function contacts() {
 
 
     return `
-    <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
-
-    <ul id="myUL">
-      <li><a href="#">Adele</a></li>
-      <li><a href="#">Agnes</a></li>
-    
-      <li><a href="#">Billy</a></li>
-      <li><a href="#">Bob</a></li>
-    
-      <li><a href="#">Calvin</a></li>
-      <li><a href="#">Christina</a></li>
-      <li><a href="#">Cindy</a></li>
-    </ul>`;
+    `;
 }
