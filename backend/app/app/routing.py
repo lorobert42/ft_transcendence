8cc09cce8f6fdade8c752a0a48569da53da78f97
lorobert <lorobert@student.42lausanne.ch>
2024-04-18 11:29:51 +0200

    Websocket authentication

--- backend/app/app/routing.py
+++ backend/app/app/routing.py
@@ -1,26 +1,13 @@
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+from .middleware import JwtAuthMiddlewareStack
 import game.routing
 
-print("ASGI SETTINGS_GAME: ", game.routing.websocket_urlpatterns)
-
-"""
 application = ProtocolTypeRouter({
-  'http': get_asgi_application(),
-  'websocket': TokenAuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-
-})
-"""
-application = ProtocolTypeRouter({
-	  'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
+	'http': get_asgi_application(),
+    'websocket': JwtAuthMiddlewareStack(
         URLRouter(
             game.routing.websocket_urlpatterns
         )
