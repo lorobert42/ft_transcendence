8e499ec54569249bb8b07cc462aeb973cc634a7b
trubat-d <theotime2015@gmail.com>
2024-04-18 11:29:39 +0200

    added tournament structure

--- frontend/public/pages/tournament.js
+++ frontend/public/pages/tournament.js
@@ -1,3 +1,25 @@
 export default function tournament() {
-    return `<div id="bracket" class="row"></div>`;
+    return `
+    <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6">
+        <h2>Round 1</h2>
+        <ul id="round1-list" class="list-group">
+      </div>
+      <div class="col-md-6">
+        <h2>Round 2</h2>
+        <ul id="round2-list" class="list-group">
+        </ul>
+      </div>
+      <div class="col-md-6">
+        <h2>Round 3</h2>
+        <ul id="round1-list" class="list-group">
+      </div>
+      <div class="col-md-6">
+        <h2>Round 4</h2>
+        <ul id="round2-list" class="list-group">
+        </ul>
+      </div>
+    </div>
+  </div>`;
 }
