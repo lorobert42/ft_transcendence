ef99f6703a9b44fcfade97dbf9ec71965b2bb289
Maxime <matchmatchandmatch@gmail.com>
2024-04-21 16:56:42 +0200

    added the bot

--- backend/app/game/consumers.py
+++ backend/app/game/consumers.py
@@ -5,6 +5,7 @@ import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import async_to_sync
 from .game import GameClass, Ball, Paddle
+import random
 
 from icecream import ic
 
@@ -16,15 +17,24 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
     game_tab = dict()
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.save_ball_x = 0
+        self.save_ball_y = 0
 
     """ All the management of the websocket. """
     async def connect(self):
         """ Comportement of the websocket when created. """
         self.room_id = self.scope['url_route']['kwargs']['id']
         self.game_room_group = 'game_%s' % self.room_id
-
+        # Determine the player types for this game instance
+        
+        #player_1_type = self.scope['url_route']['kwargs'].get('player_1_type', 'human')
+        #player_2_type = self.scope['url_route']['kwargs'].get('player_2_type', 'human')
+        #print("Player 1 type: ", player_1_type)
+        #print("Player 2 type: ", player_2_type)
+        
+        player_1_type, player_2_type = "bot", "human"
         if GameRoomConsumer.game_tab.get(self.room_id) == None:
-            GameRoomConsumer.game_tab[self.room_id] = GameClass(self.room_id)
+            GameRoomConsumer.game_tab[self.room_id] = GameClass(self.room_id, player_1_type, player_2_type)
 
         print("User connectiton")
         ic(self.scope['user'].email)
@@ -58,14 +68,32 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         """ Comportement of the class when a certain message is send from the frontend. """
         td_json = json.loads(text_data)
+
+        game_instance = GameRoomConsumer.game_tab[self.room_id]
+        if game_instance.p1_type == 'bot':
+            GameRoomConsumer.game_tab[self.room_id].p1['name'] = "bot"
+            GameRoomConsumer.game_tab[self.room_id].p1['state'] = True
+        
+        if game_instance.p2_type == 'bot':
+            GameRoomConsumer.game_tab[self.room_id].p2['name'] = "bot"
+            GameRoomConsumer.game_tab[self.room_id].p2['state'] = True
+
         if 'join' in td_json:
             await self.add_user(td_json['join'])
+            
+            
+            
         if 'local' in td_json:
-            await self.handle_move_local(td_json['local'])
+            if ((td_json['local'] == 'P1_UP' or td_json['local'] == 'P1_DOWN') and game_instance.p1_type != 'bot'):
+                await self.handle_move_local(td_json['local'])
+            elif ((td_json['local'] == 'P2_UP' or td_json['local'] == 'P2_DOWN') and game_instance.p2_type != 'bot'):
+                await self.handle_move_local(td_json['local'])            
         if 'move' in td_json:
             await self.handle_move_online(td_json['move'])
         if 'start' in td_json:
             await self.handle_start(td_json['start'])
+        # If player is a bot initialize and mark as ready
+
 
     async def handle_move_online(self, move):
         print(self.scope['user'].email, "Direction", move)
@@ -101,6 +129,7 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
             GameRoomConsumer.game_tab[self.room_id].p2['state'] = True
 
     async def handle_move_local(self, message):
+        print("Entering handle_move_local")
         if message == "P1_UP":
             await self.handle_paddle_move(KEY_P1_UP)
             print("P1 UP")
@@ -137,9 +166,58 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
             GameRoomConsumer.game_tab[self.room_id].paddle_r.move(up=False)
 
     async def loop(self, max_score):
+        print("p1 type: ", GameRoomConsumer.game_tab[self.room_id].p1_type)
+        print("p2 type: ", GameRoomConsumer.game_tab[self.room_id].p2_type)
+        self.hit_paddle = False
+        self.reset_game = True
+        self.hit_ceiling = False
         """ Main loop that will run the Game. """
         print("in loop")
-        while GameRoomConsumer.game_tab[self.room_id].active is True:
+        while GameRoomConsumer.game_tab[self.room_id].active:
+            start_time = time.time()
+            if self.reset_game == True or self.hit_paddle == True:
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                print(f"Elapsed time: {elapsed_time} seconds")
+                start_time = time.time()
+
+                self.observation = self.get_observation()  # Update observation
+
+                self.reset_game = False
+                self.hit_paddle = False
+            else:
+                #self.observation[4] = GameRoomConsumer.game_tab[self.room_id].paddle_r.y / HEIGHT
+                #self.observation[5] = GameRoomConsumer.game_tab[self.room_id].paddle_l.y / HEIGHT
+                self.observation[0] = (self.save_ball_x + self.observation[2]) / WIDTH
+                self.observation[1] = (self.save_ball_y + self.observation[3]) / HEIGHT
+                self.save_ball_x = self.observation[0] * WIDTH
+                self.save_ball_y = self.observation[1] * HEIGHT
+
+                if self.hit_ceiling == True:
+                    self.observation[3] = -self.observation[3]
+                    self.hit_ceiling = False
+
+            # Decide action for player 1 if bot
+            if GameRoomConsumer.game_tab[self.room_id].p1_type == 'bot':
+                action = self.decide_bot_action('left', self.observation)
+                await self.handle_paddle_move(action)
+                #self.observation[4] += (10 / HEIGHT)
+                if (action == KEY_P1_UP):
+                    self.observation[5] -= (10 / HEIGHT)
+                elif (action == KEY_P1_DOWN):
+                    self.observation[5] += (10 / HEIGHT)
+            
+            # Decide action for player 2 if bot
+            if GameRoomConsumer.game_tab[self.room_id].p2_type == 'bot':
+                action = self.decide_bot_action('right', self.observation)
+                await self.handle_paddle_move(action)
+                print("Action: ", action)
+
+                if (action == KEY_P2_UP):
+                    self.observation[4] -= (10 / HEIGHT)
+                elif (action == KEY_P2_DOWN):
+                    self.observation[4] += (10 / HEIGHT)
+
             if GameRoomConsumer.game_tab[self.room_id].score_p1 < GameRoomConsumer.game_tab[self.room_id].max_score and \
             GameRoomConsumer.game_tab[self.room_id].score_p2 < GameRoomConsumer.game_tab[self.room_id].max_score:
                 """ Game logic """
@@ -149,6 +227,8 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
                 right = GameRoomConsumer.game_tab[self.room_id].paddle_r
                 if ball.y + ball.rad >= HEIGHT or ball.y - ball.rad <= 0: # Ball hit the ceiling
                     GameRoomConsumer.game_tab[self.room_id].ball.y_vel *= -1
+                    self.hit_ceiling = True
+                    
                 # Cheking the direction of the ball
                 elif ball.x_vel < 0:
                     # Ball goes right to left <-
@@ -162,6 +242,7 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
                         GameRoomConsumer.game_tab[self.room_id].ball.y_vel = -1 * y_vel
                         # Will change the velocity of the ball depending of where the
                         # Ball is hitting the paddle
+                        self.hit_paddle = True
                 else:
                     # Ball goes left to right ->
                     if ball.y >= right.y and ball.y <= right.y + right.height and ball.x + ball.rad >= right.x:
@@ -174,13 +255,15 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
                         GameRoomConsumer.game_tab[self.room_id].ball.y_vel = -1 * y_vel
                         # Will change the velocity of the ball depending of where the
                         # Ball is hitting the paddle
+                        self.hit_paddle = True
                 if ball.x <= 0: # right have scored so P2 won a point
                     GameRoomConsumer.game_tab[self.room_id].score_p2 += 1
                     GameRoomConsumer.game_tab[self.room_id].ball.reset()
+                    self.reset_game = True
                 elif ball.x >= WIDTH: # left have scored so P1 won a point
                     GameRoomConsumer.game_tab[self.room_id].score_p1 += 1
                     GameRoomConsumer.game_tab[self.room_id].ball.reset()
-
+                    self.reset_game = True
                 """ Getting data to send to the front """
                 data = {
                     "P1": GameRoomConsumer.game_tab[self.room_id].paddle_l.pos(),
@@ -201,6 +284,48 @@ class GameRoomConsumer(AsyncWebsocketConsumer):
                 print("Game ended, end of the main loop.")
                 GameRoomConsumer.game_tab[self.room_id].active = False
 
+    def get_observation(self):
+        self.save_ball_y = GameRoomConsumer.game_tab[self.room_id].ball.y
+        self.save_ball_x = GameRoomConsumer.game_tab[self.room_id].ball.x
+        observation = [GameRoomConsumer.game_tab[self.room_id].ball.x / WIDTH, 
+                       GameRoomConsumer.game_tab[self.room_id].ball.y / HEIGHT, 
+                       GameRoomConsumer.game_tab[self.room_id].ball.x_vel, 
+                       GameRoomConsumer.game_tab[self.room_id].ball.y_vel, 
+                       GameRoomConsumer.game_tab[self.room_id].paddle_r.y / HEIGHT,
+                       GameRoomConsumer.game_tab[self.room_id].paddle_l.y / HEIGHT]
+
+        return (observation)
+
+    def decide_bot_action(self, side, observation):
+        random_number = random.uniform(0.03, 0.15)
+
+        if (side == "right"):
+            if (observation[2] < 0): # Ball goes to the opposite side
+                # recenter the paddle
+                if (observation[4] + random_number) * HEIGHT < HEIGHT / 2:
+                    return KEY_P2_DOWN
+                elif (observation[4] + random_number) * HEIGHT > HEIGHT / 2:
+                    return KEY_P2_UP
+                #return None
+            if observation[1] - random_number < observation[4]:
+                return KEY_P2_UP
+            elif observation[1] - random_number > observation[4]:
+                return KEY_P2_DOWN
+            return None
+        elif (side == "left"):
+            if (observation[2] > 0):
+                # recenter the paddle
+                if (observation[5] + random_number) * HEIGHT < HEIGHT / 2:
+                    return KEY_P1_DOWN
+                elif (observation[5] + random_number) * HEIGHT > HEIGHT / 2:
+                    return KEY_P1_UP
+                #return None
+            if observation[1] - random_number < observation[5]:
+                return KEY_P1_UP
+            elif observation[1] - random_number > observation[5]:
+                return KEY_P1_DOWN
+            return None
+    
     async def send_state(self, event):
         """ Function that send state of the curent game. """
         state = event['state']
