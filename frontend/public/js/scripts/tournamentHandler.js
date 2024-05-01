431d7bd792c8beed1175a1bff571c08d3f332e6a
trubat-d <theotime2015@gmail.com>
2024-05-01 23:23:35 +0200

    need some testing but json data should be okish

--- frontend/public/js/scripts/tournamentHandler.js
+++ frontend/public/js/scripts/tournamentHandler.js
@@ -1,62 +1,51 @@
 export async function tournamentHandler(dataDict = {}) {
 
   class Slot {
-    #player;
-    #score;
 
-    constructor(player, score, round, game, previousRound1 = null, previousRound2 = null) {
+    constructor(player1, player2, score1, score2, round, game, status) {
+      this.player1 = player1;
+      this.player2 = player2;
+      this.score1 = score1;
+      this.score2 = score2;
       this.round = round;
       this.game = game;
-      this.#player = player;
-      this.#score = score;
-      this.previousRound1 = previousRound1;
-      this.previousRound2 = previousRound2;
-      this.li = document.createElement('li');
-      this.li.className = "fixed-size-list-item d-flex justify-content-between align-items-center";
-      if (!this.#player && !this.previousRound1 && !this.previousRound2)
-        this.li.className += " bg-secondary";
-      this.li.innerText = "";
-
-      if (this.round == 0 && this.#player && this.player.status === "accepted") {
-        this.li.innerText = player.name;
-        this.span = document.createElement('span');
-        this.span.className = "badge text-bg-primary rounded-pill";
-        this.span.innerText = this.#score;
-        this.li.appendChild(this.span);
-      }
-      if (round) {
-        if (this.#player && this.player.status === "accepted") {
-          this.li.innerText = player.name;
-        } else if (this.previousRound1 && this.previousRound2 && this.previousRound1.player == null && this.previousRound2.player == null) {
-          this.li.innerText = "Waiting for previous round...";
-        } else if (this.previousRound1 && this.previousRound2 && this.previousRound1.player && this.previousRound2.player) {
-          this.li.innerText = `${this.previousRound1.player.name} - ${this.previousRound2.player.name}`;
-        }
-      }
+      this.status = status;
     }
 
-    set score(score) {
-      this.#score = score;
-      if (this.#player && this.player.status === "accepted") {
-        this.span.innerText = score;
+    updateSlot(player1, player2, score1, score2, round, game, status) {
+      this.player1 = player1;
+      this.player2 = player2;
+      this.score1 = score1;
+      this.score2 = score2;
+      this.round = round;
+      this.game = game;
+      this.status = status;
+    }
+
+    template() {
+      let li = document.createElement('li');
+      li.className = "fixed-size-list-item d-flex justify-content-between align-items-center";
+      if (this.status == "null") {
+        li.className += " border-0";
       }
-    }
 
-    get score() {
-      return this.#score;
-    }
+      let leftdiv = document.createElement('div');
+      leftdiv.className = "d-flex justify-content-between align-items-center";
+      let rightdiv = document.createElement('div');
+      rightdiv.className = "d-flex justify-content-between align-items-center";
 
-    set player(player) {
-      this.#player = player;
-      if (this.#player && this.player.status === "accepted") {
-        this.li.innerText = player.name;
-      } else {
-        this.li.innerText = "";
+      if (this.status == "pending") {
+        leftdiv.innerText = this.player1 ? this.player1.name : "...";
+        rightdiv.innerText = this.player2 ? this.player2.name : "...";
+      } else if (this.status == "finished" || this.status == "running") {
+        leftdiv.innerText = this.player1.name;
+        rightdiv.innerText = this.player2.name;
       }
-    }
 
-    get player() {
-      return this.#player;
+      li.appendChild(leftdiv);
+      li.appendChild(rightdiv);
+
+      return li;
     }
 
   }
@@ -141,24 +130,24 @@ export async function tournamentHandler(dataDict = {}) {
       console.log(data);
     }
 
+    const round1Bracket = document.getElementById('round1-list');
+    const round2Bracket = document.getElementById('round2-list');
+    const round3Bracket = document.getElementById('round3-list');
+    const roundBrackets = [round1Bracket, round2Bracket, round3Bracket];
 
     const data = JSON.parse(JSON.stringify(
       {
         "1,1": { player1: "user1", player2: "user5", score1: 5, score2: 1, status: "finished" },
-        "1,2": { player1: "user2", player2: "user6", score1: 2, score2: 1, status: "running" },
+        "1,2": { player1: "user2", player2: "user6", score1: 2, score2: 1, status: "pending" },
         "1,3": { player1: "user3", player2: "user7", score1: 1, score2: 2, status: "running" },
-        "1,4": { player1: "user4", player2: "user8", score1: 2, score2: 1, status: "running" },
+        // "1,4": { player1: "user4", player2: "user8", score1: 2, score2: 1, status: "running" },
         "2,1": { player1: null, player2: null, score1: 2, score2: 1, status: "pending" },
-        "2,2": { player1: null, player2: null, score1: 2, score2: 1, status: "pending" },
+        // "2,2": { player1: null, player2: null, score1: 2, score2: 1, status: "pending" },
         "3,1": { player1: null, player2: null, score1: 2, score2: 1, status: "pending" },
       }
     ));
 
     console.log(data);
-    const round1Bracket = document.getElementById('round1-list');
-    const round2Bracket = document.getElementById('round2-list');
-    const round3Bracket = document.getElementById('round3-list');
-    const round4Bracket = document.getElementById('round4-list');
 
 
     let games = [];
@@ -183,48 +172,112 @@ export async function tournamentHandler(dataDict = {}) {
       });
     }
 
-    // generateBrackets(userList, dataDict);
-    let head = new Slot(null, 0, Object.keys(data).length > 4 ? 3 : 2, 1);
-    initTree(head, 3, 1, data);
-    console.log(head);
+    let matrix = [[], [], []];
 
-    generateUsingTree(head);
+    function getPositionTranslation(round, game) {
+      if (round == 1) {
+        return [0, (game * 4) - 4];
+      } else if (round == 2) {
+        return [1, (game * 8) - 6];
+      } else if (round == 3) {
+        return [2, (game * 16) - 10];
+      }
+      return null;
+    }
+
+    function fillMatrix(matrixContent) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 14; j++) {
+          let newSlot = new Slot(null, null, 0, 0, -1, -1, "null");
+          matrixContent[i].push(newSlot);
+        }
+      }
+      return matrixContent;
+    }
 
     function getData(dataContent, round, game) {
+      console.log(round, game)
       if (!dataContent[`${round},${game}`])
         return null;
       return dataContent[`${round},${game}`];
     }
 
     function getPlayerDataFromName(name) {
+      if (!name)
+        return null;
       return usersData.find((user) => user.name === name);
     }
+    // generateBrackets(userList, dataDict);
+    matrix = fillMatrix(matrix);
 
-    function initTree(slotHead, round, game, dataContent) {
-      if (round < 1)
-        return;
+    console.log(matrix);
 
-      let roundData = getData(dataContent, round, game);
-      if (roundData == null)
-        return;
-      slotHead.previousRound1 = new Slot(roundData.player1 ? getPlayerDataFromName(roundData.player1) : null,
-        roundData.score1, round - 1, game * 2 - 1);
-      initTree(slotHead.previousRound1, round - 1, game * 2 - 1, dataContent);
-
-      slotHead.previousRound2 = new Slot(roundData.player2 ? getPlayerDataFromName(roundData.player2) : null,
-        roundData.score2, round - 1, game * 2);
-      initTree(slotHead.previousRound2, round - 1, game * 2, dataContent);
+    let headRound = 0;
+    for (let i in data) {
+      if (data[i].player1 != null)
+        headRound++;
+      if (data[i].player2 != null)
+        headRound++;
     }
-    function generateUsingTree(slotHead) {
-      console.log(slotHead);
-      if (slotHead.round == 3) {
-        for (let i = 0; i < (8 * 2) - 1; i++) {
-          if ((i % 16) - 7 == 0) {
-            round4Bracket.appendChild(slotHead.li);
-          } else
-            round4Bracket.appendChild(new Slot(null, 0, 0, 0).li);
+    headRound = headRound > 4 ? 3 : 2;
+    console.log("rounds", headRound);
+
+    for (let gameKey in data) {
+      let round = parseInt(gameKey.split(",")[0]);
+      let game = parseInt(gameKey.split(",")[1]);
+      let dataPlayer1 = data[gameKey].player1;
+      let dataPlayer2 = data[gameKey].player2;
+      let dataScore1 = data[gameKey].score1;
+      let dataScore2 = data[gameKey].score2;
+      let dataStatus = data[gameKey].status;
+
+      let pos = getPositionTranslation(round, game);
+      console.log("Pos", data, game, pos);
+      let slot = matrix[pos[0]][pos[1]];
+      slot.updateSlot(dataPlayer1 ? getPlayerDataFromName(dataPlayer1) : null,
+        dataPlayer2 ? getPlayerDataFromName(dataPlayer2) : null,
+        dataScore1, dataScore2, round, game, dataStatus);
+    }
+    // let headCoord = getPositionTranslation(headRound, 1);
+    // let head = matrix[headCoord[0]][headCoord[1]];
+    // let dataPlayer = getData(data, headRound, 1);
+    // console.log("VAl ", dataPlayer);
+    // head.inputPlayer(dataPlayer ? getPlayerDataFromName(dataPlayer.player1) : null, 0, headRound, 1, true);
+    // initTree(head, headRound, 1, data);
+    generateUsingTree(matrix);
+
+    // function initTree(slotHead, round, game, dataContent) {
+    //   if (round < 1)
+    //     return;
+
+    //   let roundData = getData(dataContent, round, game);
+    //   console.log("Data", round, game, roundData);
+    //   if (roundData == null)
+    //     return;
+
+    //   let prev1Pos = getPositionTranslation(round - 1, game * 2 - 1);
+    //   let prev2Pos = getPositionTranslation(round - 1, game * 2);
+    //   slotHead.previousRound1 = matrix[prev1Pos[0]][prev1Pos[1]];
+    //   slotHead.previousRound2 = matrix[prev2Pos[0]][prev2Pos[1]];
+
+    //   slotHead.previousRound1.inputPlayer(roundData.player1 ? getPlayerDataFromName(roundData.player1) : null,
+    //     roundData.score1, round - 1, game * 2 - 1, true);
+    //   initTree(slotHead.previousRound1, round - 1, game * 2 - 1, dataContent);
+
+
+    //   slotHead.previousRound2.inputPlayer(roundData.player2 ? getPlayerDataFromName(roundData.player2) : null,
+    //     roundData.score2, round - 1, game * 2, round - 1 == 0 && roundData.player2 == null ? false : true);
+    //   initTree(slotHead.previousRound2, round - 1, game * 2, dataContent);
+    // }
+
+    function generateUsingTree(matrixContent) {
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 14; j++) {
+          let matLi = matrixContent[i][j].template();
+          roundBrackets[i].appendChild(matLi);
         }
       }
+      return matrixContent;
     }
   }
 
