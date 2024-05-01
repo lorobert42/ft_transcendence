f84dc51ea4d79402e458ebf6bc7d1bf774bc081a
MrMobbi <julliatmathias06@gmail.com>
2024-05-01 17:27:38 +0200

    task for looking tournament started

--- frontend/public/js/scripts/localPong.js
+++ frontend/public/js/scripts/localPong.js
@@ -9,7 +9,7 @@ export function initPongGame(routerData) {
     }
 
     const gameSocket = new WebSocket(
-        'wss://' + location.host + '/ws/game/local' + localStorage.getItem('user_id') + '/?token=' + localStorage.getItem('authToken')
+        'wss://' + location.host + '/ws/game/local' + routerData.user.user_id + '/?token=' + localStorage.getItem('authToken')
     );
 
     let disconnect = false;
@@ -152,6 +152,7 @@ export function initPongGame(routerData) {
         if(window.location.pathname !== "/localroom")
         {
             clearInterval(intervalId);
+            gameSocket.close()
             return ;
         }
         drawEverything();
