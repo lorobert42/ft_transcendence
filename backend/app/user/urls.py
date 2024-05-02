30e3f11e79e162eafa9bb4137517b531f8703b5e
Stuart Rapoport <srapoport@dial-once.com>
2024-05-02 16:50:19 +0200

    with Stuart modification

--- backend/app/user/urls.py
+++ backend/app/user/urls.py
@@ -8,7 +8,9 @@ from user import views
 app_name = 'user'
 
 urlpatterns = [
-    path("create/", views.CreateUserView.as_view(), name="create"),
+    path("", views.UserListCreateView.as_view(), name="create"),
+	# path("list", views.ListUsersView.as_view(), name='user-list'),
+
     path("login/", views.LoginUserView.as_view(), name="login"),
     path("otp/activation/", views.OTPEnableRequestView.as_view(), name="enable-otp"),
     path("otp/activation/confirm/", views.OTPEnableConfirmView.as_view(), name="enable-otp-confirm"),
@@ -16,5 +18,5 @@ urlpatterns = [
     path("otp/disable/", views.OTPDisableView.as_view(), name="disable-otp"),
     path("token/refresh/", views.RefreshTokenView.as_view(), name='token_refresh'),
     path("me/", views.ManageUserView.as_view(), name="me"),
-    path('users/', views.ListUsersView.as_view(), name='user-list'),
+
 ]
