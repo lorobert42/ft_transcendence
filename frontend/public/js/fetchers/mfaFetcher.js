c4cf49615b70e3c840e3c2822634c4095e74a848
Maxime <matchmatchandmatch@gmail.com>
2024-05-05 14:48:28 +0200

    translated profile settings

--- frontend/public/js/fetchers/mfaFetcher.js
+++ frontend/public/js/fetchers/mfaFetcher.js
@@ -1,9 +1,30 @@
 import pageRouting from "../../changeContent.js";
 import { printError, printMessage, printSuccess } from "../utils/toastMessage.js";
 import { getRefreshToken } from "./usersFetcher.js";
+import { getLang } from "../utils/getLang.js";
 
 export async function requestMfaActivation(email, password) {
-  return fetch("/api/mfa/activation/", {
+	const lang = getLang();
+
+	let langdict = JSON.parse(`
+	  {
+		"FR": {
+		  "Invalidcredentials": "Identifiants invalides",
+		  "Successful": "Connexion réussie",
+		  "Unable": "Impossible de traiter votre demande, veuillez réessayer."
+		 },
+		"EN": {
+		  "Invalidcredentials": "Invalid credentials",
+		  "Successful": "Login Successful",
+		  "Unable": "Unable to process your request, please retry."
+		},
+		"PT": {
+		  "Invalidcredentials": "Credenciais inválidas",
+		  "Successful": "Login bem-sucedido",
+		  "Unable": "Incapaz de processar sua solicitação, por favor, tente novamente."
+		}
+	}`);
+	return fetch("/api/mfa/activation/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -13,7 +34,7 @@ export async function requestMfaActivation(email, password) {
   })
     .then((response) => {
       if (response.status === 401) {
-        throw new Error('Invalid credentials');
+        throw new Error(`${langdict[lang]["Invalidcredentials"]}`);
       }
       return response.json()
     })
@@ -23,7 +44,7 @@ export async function requestMfaActivation(email, password) {
         history.pushState({}, '', '/enable-otp');
         pageRouting({ 'qr_code': data.qr_code });
       } else {
-        throw new Error('Unable to process your request, please retry.');
+        throw new Error(`${langdict[lang]["Unable"]}`);
       }
     })
     .catch((error) => {
@@ -32,7 +53,27 @@ export async function requestMfaActivation(email, password) {
 }
 
 export async function confirmMfaActivation(id, otp) {
-  return fetch("/api/mfa/activation/confirm/", {
+	const lang = getLang();
+
+	let langdict = JSON.parse(`
+	  {
+		"FR": {
+		  "Invalidtoken": "Token invalide",
+		  "Successful": "Connexion réussie",
+		  "Unable": "Impossible de traiter votre demande, veuillez réessayer."
+		 },
+		"EN": {
+		  "Invalidtoken": "Invalid token",
+		  "Successful": "Login Successful",
+		  "Unable": "Unable to process your request, please retry."
+		},
+		"PT": {
+		  "Invalidtoken": "Token inválido",
+		  "Successful": "Login bem-sucedido",
+		  "Unable": "Incapaz de processar sua solicitação, por favor, tente novamente."
+		}
+	}`);
+ return fetch("/api/mfa/activation/confirm/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -42,7 +83,7 @@ export async function confirmMfaActivation(id, otp) {
   })
     .then((response) => {
       if (response.status === 401) {
-        throw new Error('Invalid token');
+        throw new Error(`${langdict[lang]["Invalidtoken"]}`);
       }
       return response.json()
     })
@@ -53,7 +94,30 @@ export async function confirmMfaActivation(id, otp) {
 }
 
 export async function disableMfa(email, password, otp) {
-  return fetch("/api/mfa/disable/", {
+	const lang = getLang();
+
+	let langdict = JSON.parse(`
+	  {
+		"FR": {
+		  "Invalidcredentials": "Identifiants invalides",
+		  "Successful": "Connexion réussie",
+		  "diableauth": "Authentification à deux facteurs désactivée",
+		  "Unable": "Impossible de traiter votre demande, veuillez réessayer."
+		 },
+		"EN": {
+		  "Invalidcredentials": "Invalid credentials",
+		  "Successful": "Login Successful",
+		  "diableauth": "Two-Factor Authentication disabled",
+		  "Unable": "Unable to process your request, please retry."
+		},
+		"PT": {
+		  "Invalidcredentials": "Credenciais inválidas",
+		  "Successful": "Login bem-sucedido",
+		  "diableauth": "Autenticação em duas etapas desativada",
+		  "Unable": "Incapaz de processar sua solicitação, por favor, tente novamente."
+		}
+	}`);
+ return fetch("/api/mfa/disable/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -62,15 +126,15 @@ export async function disableMfa(email, password, otp) {
   })
     .then((response) => {
       if (response.status === 401) {
-        throw new Error('Invalid credentials');
+        throw new Error(`${langdict[lang]["Invalidcredentials"]}`);
       }
       return response.json()
     })
     .then(async (data) => {
       if (Object.hasOwn(data, "success") && data.success === true) {
-        printMessage('Two-Factor Authentication disabled');
+        printMessage(`${langdict[lang]["diableauth"]}`);
       } else {
-        throw new Error('Unable to process your request, please retry.');
+        throw new Error(`${langdict[lang]["Unable"]}`);
       }
     })
     .catch((error) => {
@@ -79,7 +143,27 @@ export async function disableMfa(email, password, otp) {
 }
 
 export async function checkOTP(id, otp) {
-  return fetch("/api/mfa/check/", {
+	const lang = getLang();
+
+	let langdict = JSON.parse(`
+	  {
+		"FR": {
+		  "Invalidcredentials": "Identifiants invalides",
+		  "Successful": "Connexion réussie",
+		  "Unable": "Impossible de traiter votre demande, veuillez réessayer."
+		 },
+		"EN": {
+		  "Invalidcredentials": "Invalid credentials",
+		  "Successful": "Login Successful",
+		  "Unable": "Unable to process your request, please retry."
+		},
+		"PT": {
+		  "Invalidcredentials": "Credenciais inválidas",
+		  "Successful": "Login bem-sucedido",
+		  "Unable": "Incapaz de processar sua solicitação, por favor, tente novamente."
+		}
+	}`);
+ return fetch("/api/mfa/check/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -88,7 +172,7 @@ export async function checkOTP(id, otp) {
   })
     .then((response) => {
       if (response.status === 401) {
-        throw new Error('Invalid credentials');
+        throw new Error(`${langdict[lang]["Invalidcredentials"]}`);
       }
       return response.json()
     })
@@ -100,7 +184,7 @@ export async function checkOTP(id, otp) {
         history.pushState({}, '', '/home');
         pageRouting();
       } else {
-        throw new Error('Unable to process your request, please retry.');
+        throw new Error(`${langdict[lang]["Unable"]}`);
       }
     })
     .catch((error) => {
